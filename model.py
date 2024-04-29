1f8284e67ba317d944d7e7750406fc4339d78ce5
Monisha <129486568+MonishaKarthikeyan07@users.noreply.github.com>
2024-04-29 11:33:24 +0530

    model.py

--- model.py
+++ model.py
@@ -1,5 +1,4 @@
 import torch
-import numpy as np
 import torch.nn as nn
 
 class AConvBlock(nn.Module):
@@ -26,19 +25,28 @@ class tConvBlock(nn.Module):
     def __init__(self):
         super(tConvBlock, self).__init__()
 
-        block = [nn.Conv2d(6, 8, 3, padding=1, dilation=1)]
-        block += [nn.PReLU()]
-        block += [nn.Conv2d(8, 8, 3, padding=2, dilation=2)]
-        block += [nn.PReLU()]
-        block += [nn.Conv2d(8, 8, 3, padding=5, dilation=5)]
-        block += [nn.PReLU()]
-
-        block += [nn.Conv2d(8, 3, 3, padding=1)]
-        block += [nn.PReLU()]
-        self.block = nn.Sequential(*block)
+        self.conv1 = nn.Conv2d(6, 8, 3, padding=1, dilation=1)
+        self.conv2 = nn.Conv2d(8, 8, 3, padding=2, dilation=2)
+        self.conv3 = nn.Conv2d(8, 8, 3, padding=5, dilation=5)
+        self.conv4 = nn.Conv2d(8, 3, 3, padding=1)
+        self.prelu = nn.PReLU()
 
     def forward(self, x):
-        return self.block(x)
+        x1 = self.conv1(x)
+        x1 = self.prelu(x1)
+
+        x2 = self.conv2(x1)
+        x2 = self.prelu(x2)
+
+        x3 = self.conv3(x2)
+        x3 = self.prelu(x3)
+
+        x = torch.cat((x * 0 + x3, x), 1)
+
+        x = self.conv4(x)
+        x = self.prelu(x)
+
+        return x
 
 class PhysicalNN(nn.Module):
     def __init__(self):
