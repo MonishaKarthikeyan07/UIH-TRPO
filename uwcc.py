dce39ff74acca18280f7c148b564b83e95bc0e21
Monisha <129486568+MonishaKarthikeyan07@users.noreply.github.com>
2024-04-29 09:08:35 +0530

    uwcc.py

--- uwcc.py
+++ uwcc.py
@@ -13,14 +13,14 @@ def get_imgs_list(ori_dirs, ucc_dirs):
         img_name = os.path.splitext(os.path.basename(ori_imgdir))[0]
         ucc_imgdir = os.path.join(os.path.dirname(ucc_dirs[0]), img_name + '.png')
 
-        if os.path.exists(ucc_imgdir):
+        if ucc_imgdir in ucc_dirs:
             img_list.append((ori_imgdir, ucc_imgdir))
 
     return img_list
 
-class UWCCDataset(data.Dataset):
+class uwcc(data.Dataset):
     def __init__(self, ori_dirs, ucc_dirs, train=True, loader=img_loader):
-        super(UWCCDataset, self).__init__()
+        super(uwcc, self).__init__()
 
         self.img_list = get_imgs_list(ori_dirs, ucc_dirs)
         if len(self.img_list) == 0:
