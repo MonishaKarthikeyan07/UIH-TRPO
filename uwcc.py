8d429e1ec1d31ed6b300feba9a074ce22e789360
Monisha <129486568+MonishaKarthikeyan07@users.noreply.github.com>
2024-04-29 21:54:53 +0530

    uwcc.py

--- uwcc.py
+++ uwcc.py
@@ -1,55 +1,44 @@
-
-import torch
+import torch.utils.data as data
 import os
 from PIL import Image
-from torch.utils.data import Dataset
+from torchvision import transforms
 
 def img_loader(path):
     img = Image.open(path)
     return img
 
-def get_imgs_list(ori_dirs,ucc_dirs):
+def get_imgs_list(ori_dirs, ucc_dirs):
     img_list = []
     for ori_imgdir in ori_dirs:
-        img_name = (ori_imgdir.split('/')[-1]).split('.')[0]
-        ucc_imgdir = os.path.dirname(ucc_dirs[0])+'/'+img_name+'.png'
+        img_name = os.path.splitext(os.path.basename(ori_imgdir))[0]
+        ucc_imgdir = os.path.join(os.path.dirname(ucc_dirs[0]), img_name + '.png')
 
         if ucc_imgdir in ucc_dirs:
-            img_list.append(tuple([ori_imgdir,ucc_imgdir]))
+            img_list.append((ori_imgdir, ucc_imgdir))
 
     return img_list
 
-class UWCCDataset(Dataset):
+class UWCCDataset(data.Dataset):
     def __init__(self, ori_dirs, ucc_dirs, train=True, loader=img_loader):
-        super(uwcc, self).__init__()
+        super(UWCCDataset, self).__init__()
 
         self.img_list = get_imgs_list(ori_dirs, ucc_dirs)
         if len(self.img_list) == 0:
-            raise(RuntimeError('Found 0 image pairs in given directories.'))
+            raise RuntimeError('Found 0 image pairs in given directories.')
 
         self.train = train
         self.loader = loader
 
-        if self.train == True:
-            print('Found {} pairs of training images'.format(len(self.img_list)))
+        if self.train:
+            print(f'Found {len(self.img_list)} pairs of training images')
         else:
-            print('Found {} pairs of testing images'.format(len(self.img_list)))
+            print(f'Found {len(self.img_list)} pairs of testing images')
             
     def __getitem__(self, index):
         img_paths = self.img_list[index]
         sample = [self.loader(img_paths[i]) for i in range(len(img_paths))]
 
-        if self.train == True:
-            oritransform = transforms.Compose([
-                # Data augmentation transforms for training
-                transforms.ToTensor(),
-            ])
-            ucctransform = transforms.Compose([
-                transforms.ToTensor(),
-            ])
-            sample[0] = oritransform(sample[0])
-            sample[1] = ucctransform(sample[1])
-        else:
+        if self.train:
             oritransform = transforms.Compose([
                 transforms.ToTensor(),
             ])
