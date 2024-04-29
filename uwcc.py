e32473ee367e1a7027ebaa64d89156b43077e343
Monisha <129486568+MonishaKarthikeyan07@users.noreply.github.com>
2024-04-29 12:28:46 +0530

    uwcc.py

--- uwcc.py
+++ uwcc.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 from PIL import Image
+import torch
 from torch.utils.data import Dataset
 
 class uwcc(Dataset):
@@ -8,42 +9,29 @@ class uwcc(Dataset):
         self.ori_dirs = ori_dirs
         self.ucc_dirs = ucc_dirs
         self.train = train
-        self.image_pairs = self.find_image_pairs()
+        self.ori_paths = []
+        self.ucc_paths = []
 
-        if len(self.image_pairs) == 0:
-            print("Warning: Found 0 image pairs in given directories.")
+        self._make_paths()
+        if len(self.ori_paths) == 0 or len(self.ucc_paths) == 0:
+            raise RuntimeError('Found 0 image pairs in given directories.')
 
     def __len__(self):
-        return len(self.image_pairs)
+        return len(self.ori_paths)
 
-    def __getitem__(self, index):
-        ori_path, ucc_path = self.image_pairs[index]
-        ori_image = Image.open(ori_path)
-        ucc_image = Image.open(ucc_path)
+    def __getitem__(self, idx):
+        ori_array = np.array(Image.open(self.ori_paths[idx]))
+        ucc_array = np.array(Image.open(self.ucc_paths[idx]))
 
-        # Convert images to numpy arrays and normalize
-        ori_array = np.array(ori_image) / 255.0
-        ucc_array = np.array(ucc_image) / 255.0
-
-        # Convert arrays to tensors
+        # Convert images to PyTorch tensors and normalize
         ori_tensor = torch.from_numpy(ori_array).permute(2, 0, 1).float()
         ucc_tensor = torch.from_numpy(ucc_array).permute(2, 0, 1).float()
 
         return ori_tensor, ucc_tensor
 
-    def find_image_pairs(self):
-        image_pairs = []
-
+    def _make_paths(self):
         for ori_dir, ucc_dir in zip(self.ori_dirs, self.ucc_dirs):
-            ori_files = os.listdir(ori_dir)
-            ucc_files = os.listdir(ucc_dir)
-
-            for ori_file in ori_files:
-                ori_path = os.path.join(ori_dir, ori_file)
-                ucc_file = ori_file.replace('ori', 'ucc')
-                ucc_path = os.path.join(ucc_dir, ucc_file)
-
-                if ucc_file in ucc_files:
-                    image_pairs.append((ori_path, ucc_path))
-
-        return image_pairs
+            ori_paths = [os.path.join(ori_dir, img) for img in os.listdir(ori_dir) if img.endswith('.png')]
+            ucc_paths = [os.path.join(ucc_dir, img) for img in os.listdir(ucc_dir) if img.endswith('.png')]
+            self.ori_paths.extend(ori_paths)
+            self.ucc_paths.extend(ucc_paths)
