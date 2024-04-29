51d40f55108ba3cbbf155715747e39d242497cb9
Monisha <129486568+MonishaKarthikeyan07@users.noreply.github.com>
2024-04-29 09:39:21 +0530

    test.py

--- test.py
+++ test.py
@@ -1,39 +1,27 @@
 import os
-import sys
 import torch
 import numpy as np
 from PIL import Image
+from model import PhysicalNN
+import argparse
 from torchvision import transforms
 import datetime
-from trpo import TRPO
-from model import PhysicalNN
 
-def main(checkpoint_path):
 
-    # Check for GPU
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+def main(checkpoint, imgs_path, result_path):
+
+    ori_dirs = []
+    for image in os.listdir(imgs_path):
+        ori_dirs.append(os.path.join(imgs_path, image))
 
     # Load model
     model = PhysicalNN()
-    model = torch.nn.DataParallel(model).to(device)
+    model.load_state_dict(torch.load(checkpoint, map_location='cpu')['state_dict'])
     print("=> loading trained model")
-    checkpoint = torch.load(checkpoint_path, map_location=device)
+    checkpoint = torch.load(checkpoint, map_location='cpu')
     model.load_state_dict(checkpoint['state_dict'])
     print("=> loaded model at epoch {}".format(checkpoint['epoch']))
-    model = model.module
     model.eval()
-
-    # TRPO instance
-    trpo = TRPO(model)
-
-    # Test image path
-    imgs_path = '/content/drive/MyDrive/Dataset/test_images/'
-
-    # Result path
-    result_path = '/content/drive/MyDrive/Dataset/results/'
-
-    # Get list of test images
-    ori_dirs = [os.path.join(imgs_path, image) for image in os.listdir(imgs_path)]
 
     testtransform = transforms.Compose([
                 transforms.ToTensor(),
@@ -42,23 +30,29 @@ def main(checkpoint_path):
 
     starttime = datetime.datetime.now()
     for imgdir in ori_dirs:
-        img_name = os.path.splitext(os.path.basename(imgdir))[0]
+        img_name = (imgdir.split('/')[-1]).split('.')[0]
         img = Image.open(imgdir)
         inp = testtransform(img).unsqueeze(0)
-        inp = inp.to(device)
-        out = trpo.predict(inp)
+        out = model(inp)
 
         corrected = unloader(out.cpu().squeeze(0))
-        dir = os.path.join(result_path, 'results_{}'.format(checkpoint['epoch']))
+        dir = '{}/results_{}'.format(result_path, checkpoint['epoch'])
         if not os.path.exists(dir):
             os.makedirs(dir)
-        corrected.save(os.path.join(dir, '{}_corrected.png'.format(img_name)))
+        corrected.save(dir+'/{}corrected.png'.format(img_name))
     endtime = datetime.datetime.now()
-    print("Total time taken:", endtime - starttime)
+    print(endtime-starttime)
+
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: python test.py CHECKPOINT_PATH")
-    else:
-        _, checkpoint_path = sys.argv
-        main(checkpoint_path)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--checkpoint', help='checkpoints path', required=True)
+    args = parser.parse_args()
+    checkpoint = args.checkpoint
+
+    # Default paths for images and results folders
+    imgs_path = './test_img/'
+    result_path = './results/'
+
+    main(checkpoint=checkpoint, imgs_path=imgs_path, result_path=result_path)
