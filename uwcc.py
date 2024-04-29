6897342e8a2c14e822d597fa6a2c3a10cd1267e5
Monisha <129486568+MonishaKarthikeyan07@users.noreply.github.com>
2024-04-29 18:04:03 +0530

    uwcc.py

--- uwcc.py
+++ uwcc.py
@@ -1,74 +1,57 @@
-import os
-from PIL import Image
+'''
+Author:Xuelei Chen(chenxuelei@hotmail.com)
+'''
 import torch
-from torch.utils.data import Dataset
-from torchvision import transforms
-
-def img_loader(path):
-    img = Image.open(path)
-    return img
-
-def get_imgs_list(ori_dirs, ucc_dirs):
-    img_list = []
-    for ori_imgdir in ori_dirs:
-        for img_file in os.listdir(ori_imgdir):
-            if img_file.endswith(('.jpg', '.jpeg', '.png', '.bmp')):
-                img_name = os.path.splitext(img_file)[0]
-                ucc_imgfile = os.path.join(os.path.dirname(ucc_dirs[0]), img_name + '.png')
-
-                if ucc_imgfile in ucc_dirs:
-                    ori_imgpath = os.path.join(ori_imgdir, img_file)
-                    img_list.append((ori_imgpath, ucc_imgfile))
-
-    return img_list
+import numpy as np
+import torch.nn as nn
 
 
+class AConvBlock(nn.Module):
+    def __init__(self):
+        super(AConvBlock,self).__init__()
 
-class UWCCDataset(Dataset):
-    def __init__(self, ori_dirs, ucc_dirs, train=True, loader=img_loader):
-        super(UWCCDataset, self).__init__()
+        block = [nn.Conv2d(3,3,3,padding = 1)]
+        block += [nn.PReLU()]
 
-        print("Original image directories:")
-        print(ori_dirs)
-        print("UCC image directories:")
-        print(ucc_dirs)
+        block += [nn.Conv2d(3,3,3,padding = 1)]
+        block += [nn.PReLU()]
 
-        self.img_list = get_imgs_list(ori_dirs, ucc_dirs)
-        if len(self.img_list) == 0:
-            raise RuntimeError('Found 0 image pairs in given directories. Please ensure that each original image has a corresponding UCC image.')
+        block += [nn.AdaptiveAvgPool2d((1,1))]
+        block += [nn.Conv2d(3,3,1)]
+        block += [nn.PReLU()]
+        block += [nn.Conv2d(3,3,1)]
+        block += [nn.PReLU()]
+        self.block = nn.Sequential(*block)
 
-        self.train = train
-        self.loader = loader
+    def forward(self,x):
+        return self.block(x)
 
-        if self.train:
-            print(f'Found {len(self.img_list)} pairs of training images')
-        else:
-            print(f'Found {len(self.img_list)} pairs of testing images')
-            
-    def __getitem__(self, index):
-        img_paths = self.img_list[index]
-        sample = [self.loader(img_paths[i]) for i in range(len(img_paths))]
+class tConvBlock(nn.Module):
+    def __init__(self):
+        super(tConvBlock,self).__init__()
 
-        if self.train:
-            oritransform = transforms.Compose([
-                transforms.ToTensor(),
-            ])
-            ucctransform = transforms.Compose([
-                transforms.ToTensor(),
-            ])
-            sample[0] = oritransform(sample[0])
-            sample[1] = ucctransform(sample[1])
-        else:
-            oritransform = transforms.Compose([
-                transforms.ToTensor(),
-            ])
-            ucctransform = transforms.Compose([
-                transforms.ToTensor(),
-            ])
-            sample[0] = oritransform(sample[0])
-            sample[1] = ucctransform(sample[1])
+        block = [nn.Conv2d(6,8,3,padding=1,dilation=1)]
+        block += [nn.PReLU()]
+        block += [nn.Conv2d(8,8,3,padding=2,dilation=2)]
+        block += [nn.PReLU()]
+        block += [nn.Conv2d(8,8,3,padding=5,dilation=5)]
+        block += [nn.PReLU()]
 
-        return sample
+        block += [nn.Conv2d(8,3,3,padding=1)]
+        block += [nn.PReLU()]
+        self.block = nn.Sequential(*block)
+    def forward(self,x):
+        return self.block(x)
 
-    def __len__(self):
-        return len(self.img_list)
+class PhysicalNN(nn.Module):
+    def __init__(self):
+        super(PhysicalNN,self).__init__()
+
+        self.ANet = AConvBlock()
+        self.tNet = tConvBlock()
+
+    def forward(self,x):
+        A = self.ANet(x)
+        t = self.tNet(torch.cat((x*0+A,x),1))
+        out = ((x-A)*t + A)
+        return torch.clamp(out,0.,1.)
