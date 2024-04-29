64ab1e0c73d3de89f8f50d88ae19730fc00224a0
Monisha <129486568+MonishaKarthikeyan07@users.noreply.github.com>
2024-04-29 13:34:44 +0530

    train.py

--- train.py
+++ train.py
@@ -1,17 +1,58 @@
-import argparse
+import os
+import torch
 from trpo import TRPOAgent
+import shutil
+import sys
 
-def main():
-    parser = argparse.ArgumentParser(description='Training TRPO Agent')
-    parser.add_argument('--ori_dirs', type=str, nargs='+', help='Directories containing original images')
-    parser.add_argument('--ucc_dirs', type=str, nargs='+', help='Directories containing UCC images')
-    parser.add_argument('--batch_size', type=int, default=32, help='Batch size for training')
-    parser.add_argument('--n_workers', type=int, default=2, help='Number of workers for data loading')
-    parser.add_argument('--epochs', type=int, default=10, help='Number of epochs for training')
-    args = parser.parse_args()
+class Trainer:
+    def __init__(self):
+        self.trpo_agent = TRPOAgent()
 
-    trpo_agent = TRPOAgent()
-    trpo_agent.train(args.ori_dirs, args.ucc_dirs, args.batch_size, args.n_workers, args.epochs)
+    def main(self, ori_dirs, ucc_dirs):
+        batch_size = 32
+        n_workers = 2
+        epochs = 50
+
+        dataloader = self.trpo_agent.collect_samples(ori_dirs, ucc_dirs, batch_size, n_workers)
+
+        for epoch in range(epochs):
+            for batch in dataloader:
+                states, actions, rewards = batch
+                loss = self.trpo_agent.train(ori_dirs, ucc_dirs, batch_size, n_workers, epochs)
+
+            # Save checkpoint
+            is_best = False  # Modify this according to your criteria
+            save_checkpoint({
+                'epoch': epoch + 1,
+                'state_dict': self.trpo_agent.policy.state_dict(),
+                'optimizer': self.trpo_agent.optimizer.state_dict(),
+            }, is_best)
+
+        print("Training complete.")
+
+def save_checkpoint(state, is_best):
+    """Saves checkpoint to disk"""
+    freq = 500
+    epoch = state['epoch'] 
+
+    filename = './checkpoints/model_tmp.pth.tar'
+    if not os.path.exists('./checkpoints'):
+        os.makedirs('./checkpoints')
+
+    torch.save(state, filename)
+
+    if epoch % freq == 0:
+        shutil.copyfile(filename, './checkpoints/model_{}.pth.tar'.format(epoch))
+    if is_best:
+        shutil.copyfile(filename, './checkpoints/model_best_{}.pth.tar'.format(epoch))
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) != 3:
+        print("Usage: python train.py TRAIN_RAW_IMAGE_FOLDER TRAIN_REFERENCE_IMAGE_FOLDER")
+        sys.exit(1)
+
+    ori_dirs = sys.argv[1]
+    ucc_dirs = sys.argv[2]
+
+    trainer = Trainer()
+    trainer.main(ori_dirs, ucc_dirs)
