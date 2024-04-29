ce0363d5d672f35c265768db1399c0f3ee3a144c
Monisha <129486568+MonishaKarthikeyan07@users.noreply.github.com>
2024-04-29 20:53:03 +0530

    model.py

--- model.py
+++ model.py
@@ -1,40 +1,54 @@
 import torch
+import numpy as np
 import torch.nn as nn
+
+class AConvBlock(nn.Module):
+    def __init__(self):
+        super(AConvBlock, self).__init__()
+
+        block = [nn.Conv2d(3, 3, 3, padding=1)]
+        block += [nn.PReLU()]
+
+        block += [nn.Conv2d(3, 3, 3, padding=1)]
+        block += [nn.PReLU()]
+
+        block += [nn.AdaptiveAvgPool2d((1, 1))]
+        block += [nn.Conv2d(3, 3, 1)]
+        block += [nn.PReLU()]
+        block += [nn.Conv2d(3, 3, 1)]
+        block += [nn.PReLU()]
+        self.block = nn.Sequential(*block)
+
+    def forward(self, x):
+        return self.block(x)
+
+class tConvBlock(nn.Module):
+    def __init__(self):
+        super(tConvBlock, self).__init__()
+
+        block = [nn.Conv2d(6, 8, 3, padding=1, dilation=1)]
+        block += [nn.PReLU()]
+        block += [nn.Conv2d(8, 8, 3, padding=2, dilation=2)]
+        block += [nn.PReLU()]
+        block += [nn.Conv2d(8, 8, 3, padding=5, dilation=5)]
+        block += [nn.PReLU()]
+
+        block += [nn.Conv2d(8, 3, 3, padding=1)]
+        block += [nn.PReLU()]
+        self.block = nn.Sequential(*block)
+
+    def forward(self, x):
+        return self.block(x)
 
 class PhysicalNN(nn.Module):
     def __init__(self):
         super(PhysicalNN, self).__init__()
-        self.block = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-        self.aconv = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(32, 32, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-        self.tconv = nn.Sequential(
-            nn.Conv2d(6, 32, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(32, 32, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-        self.fc1 = nn.Linear(32 * 64 * 64, 512)
-        self.fc2 = nn.Linear(512, 5)
-        self.softmax = nn.Softmax(dim=1)
+
+        self.ANet = AConvBlock()
+        self.tNet = tConvBlock()
 
     def forward(self, x):
-        x1 = self.block(x)
-        x2 = self.aconv(x)
-        t = torch.cat((x2, x), 1)
-        t = self.tconv(t)
-        t = t.view(t.size(0), -1)
-        t = torch.relu(self.fc1(t))
-        t = self.fc2(t)
-        return self.softmax(t)
+        A = self.ANet(x)
+        t = self.tNet(torch.cat((x * 0 + A, x), 1))
+        out = ((x - A) * t + A)
+        return torch.clamp(out, 0., 1.)
